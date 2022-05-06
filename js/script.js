c68080f78770bdffcc1f76e6ac0f6a2942ab2fce
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-05-06 09:33:59 +0000

    Added latticePaths, factorial and clearInfo functions at script.js

--- js/script.js
+++ js/script.js
@@ -15,3 +15,36 @@ function getNumberInfo() {
     // Display Information in the Browser
     document.getElementById("numinfo").innerHTML = txt;
 }
+
+/*
+    Function to return the number of paths through a given gridSize starting at
+    top left corner going to bottom right corner and being able to move right and down only.
+    latticePaths(4) returns 70
+    latticePaths(9) returns 48620
+    latticePaths(20) returns 137846528820
+*/
+function latticePaths(gridSize) {
+    gridSize = gridSize * 2;
+    let k = gridSize/2;
+    return (factorial(gridSize))/(factorial(k)*(factorial(gridSize-k)));
+}
+
+/*
+    Function to calculate the factorial of a number n
+    factorial(0) returns 1
+    factorial(3) returns 6
+*/
+function factorial(n) {
+    if (n==0) return 1;
+    let product = 1;
+    for (let i=1;i<=n;i++) {
+        product *= i;
+    }
+    return product;
+}
+
+// Function to Clear Information
+function clearInfo() {
+    let txt = "";
+    document.getElementById("numinfo").innerHTML = txt;
+}
